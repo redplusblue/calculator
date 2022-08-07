ae853cf4d63feb162d29af8f54bfeb7ceefea176
sk <samirkabra1161@gmail.com>
2022-08-07 10:37:08 -0400

    Added operators, copy to clipboard feature

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 mainDisplay = document.getElementById('main-display')
 miniDisplay = document.getElementById('mini-display')
 
-atWelcome = 'Calculator'
-
+function copyText(display){
+    text = ''
+    if(miniDisplay.innerText == '' || mainDisplay.innerText == ''){
+        alert("Cannot copy empty element!")
+    }
+    if(display == 'mini'){
+        text = miniDisplay.innerText
+        navigator.clipboard.writeText(text)
+        alert(`Copied ${text} to clipboard!`)
+    } else {
+        text = mainDisplay.innerText
+        navigator.clipboard.writeText(text)
+        alert(`Copied ${text} to clipboard!`)
+    }
+}
 
 function clearAll() {
     mainDisplay.innerText = '';
@@ -11,5 +24,59 @@ function clearAll() {
 
 function digit(d){
     mainDisplay.innerText += d;
-    miniDisplay.innerText = mainDisplay.innerText;
+    miniDisplay.innerText += d;
 }
+
+function operator(o) {
+    console.log(0)
+    if(miniDisplay.innerText.length == 0 && mainDisplay.innerText.length == 0) {
+        return
+    } else {
+        if(o == '.'){
+            if(mainDisplay.innerText.includes('.') || miniDisplay.innerText.includes('.')){
+                // FIX THIS
+                // if the number already includes . then no more but display can have more than 1 .
+                return
+            } else {
+                mainDisplay.innerText += o;
+                miniDisplay.innerText += o;
+                
+            }
+        } else {
+        mainDisplay.innerText = '';
+        miniDisplay.innerText += o;
+        }
+    }
+}
+
+function clearLast() {
+    if(mainDisplay.innerText.length == 0 && miniDisplay.innerText.length != 0){
+        let toEval = miniDisplay.innerText;
+        toEval = toEval.split('');
+        toEval[toEval.length-1] = '';
+        toEval = toEval.join('');
+        miniDisplay.innerText = toEval;
+    } else if(mainDisplay.innerText.length != 0 && miniDisplay.innerText.length == 0){
+        let toEval = mainDisplay.innerText;
+        toEval = toEval.split('');
+        toEval[toEval.length-1] = '';
+        toEval = toEval.join('')
+        mainDisplay.innerText = toEval;
+    } else if (mainDisplay.innerText.length != 0 && miniDisplay.innerText.length != 0){
+        let mainText = mainDisplay.innerText;
+        mainText = mainText.split('');
+        let miniText = miniDisplay.innerText;
+        miniText = miniText.split('');
+        mainText[mainText.length-1] = '';
+        miniText[miniText.length-1] = '';
+        mainText = mainText.join('');
+        miniText = miniText.join('');
+        miniDisplay.innerText = miniText;
+        mainDisplay.innerText = mainText;
+    } else {
+        return
+    }
+}
+
+
+
